6ff415266d198702809e039d019bbccd4f48af05
Tegardc <tegardimas2022@gmail.com>
2024-03-07 12:52:12 +0800

    Create ser/index.js, src/router/index.js, .env, Routing index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("./router");
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+// app.get("/", (req, res) => {
+//   res.send("Homepagee");
+// });
+app.listen(process.env.PORT, () => {
+  console.log("Server is Running on http://localhost:3000");
+});
