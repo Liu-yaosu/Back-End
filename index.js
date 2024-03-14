35d98541f8342dc65536d1f60850e8a46a3d6c26
Tegardc <tegardimas2022@gmail.com>
2024-03-14 14:38:49 +0800

    create login.router.js, login.controller.js, user.router.js, user.controller.js, register.router.js, register.controller., validator.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const morgan = require("morgan");
-const router = require("./router");
+const router = require("./src/router");
+
 dotenv.config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 const { Router } = require("express");
+const userRouter = require("../router/user.router");
+const regisRouter = require("../router/register.router");
+const loginRouter = require("../router/login.router");
 const router = Router();
 router.get("/", (req, res) => {
   res.json("Hello Anjing");
 });
+router.use(loginRouter);
+router.use(userRouter);
+router.use(regisRouter);
 module.exports = router;
